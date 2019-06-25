06e71a6968c7e9bbd05893668888aee25b0b7662
rantaro <genki.meado@gmail.com>
2019-06-25 13:04:45 +0900

    topのUIを調整

--- vue_card_battle/src/vews/introduction.vue
+++ vue_card_battle/src/vews/introduction.vue
@@ -12,16 +12,31 @@ export default {
   components: {},
   methods: {
     goInGame: function() {
-      this.$router.push("InGame");
+      this.$router.push('InGame')
     }
   }
-};
+}
 </script>
 
 <style scoped>
 .btn {
-  width: 200px;
-  height: 50px;
-  background: red;
+  text-align: center;
+  display: inline-block;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  background: #668ad8;/*ボタン色*/
+  color: #FFF;
+  border-bottom: solid 4px #627295;
+  border-radius: 3px;
+  width: 120px;
+  position: absolute;
+  bottom: 20px;
+  left: 110px;
+}
+.btn:active {
+  /*ボタンを押したとき*/
+  -webkit-transform: translateY(4px);
+  transform: translateY(4px);/*下に動く*/
+  border-bottom: none;/*線を消す*/
 }
 </style>
--- vue_card_battle/src/vews/top.vue
+++ vue_card_battle/src/vews/top.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
-    <div>Top</div>
-    <div>ようこそ！！！！！</div>
-    <div class="btn" @click="goInGame()">go to Top</div>
+  <div class="top">
+    <h1 class="game_title">昆虫じゃんけんカードバトル</h1>
+    <img class="top_img" src="../../assets/img/ui/top.png">
+    <h2 class="game_subtitle">~Vue.jsでDCGを作る~</h2>
+    <div class="btn" @click="goInGame()">ゲームを始める</div>
   </div>
 </template>
 
@@ -11,16 +12,48 @@ export default {
   components: {},
   methods: {
     goInGame: function() {
-      this.$router.push("/Introduction");
+      this.$router.push('/Introduction')
     }
   }
-};
+}
 </script>
 
 <style scoped>
+.top_img{
+  width: 250px;
+  position: absolute;
+  top: 160px;
+  left: 85px;
+}
+.game_title{
+  text-align: center;
+  font-size: 26px;
+}
+.game_subtitle{
+  text-align: center;
+  font-size: 22px;
+  position: absolute;
+  bottom: 60px;
+  width: 100%;
+}
 .btn {
-  width: 200px;
-  height: 50px;
-  background: red;
+  text-align: center;
+  display: inline-block;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  background: #668ad8;/*ボタン色*/
+  color: #FFF;
+  border-bottom: solid 4px #627295;
+  border-radius: 3px;
+  width: 120px;
+  position: absolute;
+  bottom: 20px;
+  left: 110px;
+}
+.btn:active {
+  /*ボタンを押したとき*/
+  -webkit-transform: translateY(4px);
+  transform: translateY(4px);/*下に動く*/
+  border-bottom: none;/*線を消す*/
 }
 </style>
